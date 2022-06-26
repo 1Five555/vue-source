75ff09a7f50d332e34452613883a2c7364acd108
烧水壶 <994871915@qq.com>
2022-06-26 22:08:01 +0800

    模板编译：baseCompile学习

--- vue-2.6.14/src/compiler/index.js
+++ vue-2.6.14/src/compiler/index.js
@@ -12,14 +12,20 @@ export const createCompiler = createCompilerCreator(function baseCompile (
   template: string,
   options: CompilerOptions
 ): CompiledResult {
+  // 把模板转换成AST抽象语法树
   const ast = parse(template.trim(), options)
   if (options.optimize !== false) {
+    // 优化抽象语法树
     optimize(ast, options)
   }
+  // 把抽象语法树生成字符串形式的JS代码
   const code = generate(ast, options)
   return {
     ast,
+    // 渲染函数
+    // 这里的render并不是最后的render函数，而是字符串形式的，最后还要通过toFunction转换成函数的形式
     render: code.render,
+    // 静态渲染函数，生成静态VNode树
     staticRenderFns: code.staticRenderFns
   }
 })
